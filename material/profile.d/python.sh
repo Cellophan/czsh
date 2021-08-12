1817c5ab5cb0f17525f3927298af462febd61070
Cell <>
2021-08-12 16:28:15 +0200

    FIX pyenv init and bash style

--- material/profile.d/python.sh
+++ material/profile.d/python.sh
@@ -1,12 +1,16 @@
 # pyenv
 PYENV_ROOT="${HOME}/.pyenv"
-chmod a+w ${PYENV_ROOT} ${PYENV_ROOT}/shims ${PYENV_ROOT}/versions
+PATH="$PYENV_ROOT/bin:$PYENV_ROOT/shims:$PATH"
+export PYENV_ROOT PATH
+
+chmod a+w \
+    ${PYENV_ROOT} ${PYENV_ROOT}/shims \
+    ${PYENV_ROOT}/versions ${PYENV_ROOT}/version
 
 if [ ! -d ${HOME}/.local/lib ]; then
-  mkdir -p ${HOME}/.local/lib
-  chown ${USER}. ${HOME}/.local/lib
+    mkdir -p ${HOME}/.local/lib
+    chown ${USER}. ${HOME}/.local/lib
 fi
 
 # poetry
 export POETRY_VIRTUALENVS_IN_PROJECT=true
-
--- material/profile.d/terraform.sh
+++ material/profile.d/terraform.sh
@@ -1,3 +1,2 @@
 : "${TF_INPUT:="0"}"
-
 export TF_INPUT
