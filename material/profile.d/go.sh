5e5def583188b4940c68046742c861d603644256
Cell <>
2022-10-26 15:33:55 +0200

    DISABLE go settings

--- material/profile.d/go.sh
+++ material/profile.d/go.sh
@@ -1,6 +1,6 @@
-: "${GOBIN:="$WORKDIR"}"
-: "${GOROOT:="/usr/local/go"}"
-# : "${GOPRIVATE:=""}"
-PATH=$PATH:${GOROOT}/bin:${GOBIN}
-export GOBIN GOROOT PATH GOPRIVATE
+# : "${GOBIN:="$WORKDIR"}"
+# : "${GOROOT:="/usr/local/go"}"
+# # : "${GOPRIVATE:=""}"
+# PATH=$PATH:${GOROOT}/bin:${GOBIN}
+# export GOBIN GOROOT PATH GOPRIVATE
 
