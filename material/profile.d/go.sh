bfe104eae5bf8ba69d10b90512dc7568ef73ae13
Cell <>
2023-02-09 13:41:17 +0100

    ADD go env vars and history

--- material/profile.d/go.sh
+++ material/profile.d/go.sh
@@ -1,7 +1,11 @@
 # https://go.dev/doc/code
+# go env
 # go env -w GOBIN=$PWD/bin
 # go env -u GOBIN
-: "${GOBIN:="$WORKDIR/bin"}"
+# go list -f '{{.Target}}' .
+# go mod tidy
+: ${GOBIN:="${WORKDIR}/bin"}
+: ${GOCACHE:="${WORKDIR}/.gocache"}
 
 # : "${GOROOT:="/usr/local/go"}"
 # # : "${GOPRIVATE:=""}"
